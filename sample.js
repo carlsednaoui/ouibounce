d0e5403b6ac372a5ebe633cbee500478665d3bf6
carl sednaoui <carlsednaoui@gmail.com>
2013-12-17 15:19:23 -0500

    That was firing tooooo soon

--- sample.js
+++ sample.js
@@ -38,8 +38,7 @@ function ouibounce() {
 
     // check if mouse is intending to exit
     // look at mouse speed and mouse direction
-    // over 50px per event emitted == FIRE!
-    if (Math.abs(lastY - event.pageY) > 40 
+    if (Math.abs(lastY - event.pageY) > 45
         && direction == 'up' 
         && scrolling === false) {
       showModal('mouse is moving fast');
