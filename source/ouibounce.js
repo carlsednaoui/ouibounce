8859eab429b3d916523e683290262030636e435a
carl sednaoui <carlsednaoui@gmail.com>
2014-01-27 20:43:14 -0500

    Add sensitivity

--- source/ouibounce.js
+++ source/ouibounce.js
@@ -1,7 +1,9 @@
-$.fn.ouibounce = function() {
-  var _this = this;
+$.fn.ouibounce = function(config) {
+  var sensitivity = config.sensitivity || 20,
+      _this       = this;
+  
   $('html').on('mouseout.ouibounce', function(e) {
-    if (!(e.clientY < 20)) return;
+    if (!(e.clientY < sensitivity)) return;
     _this.show();
     $('html').off('mouseout.ouibounce');
   });
