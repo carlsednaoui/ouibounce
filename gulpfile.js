5422cdeda9a3731143e30049fb61625d1bd95528
carl sednaoui <carlsednaoui@gmail.com>
2014-02-10 21:34:57 -0500

    Add modal example

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
-var gulp    = require('gulp'),
-    uglify  = require('gulp-uglify'),
-    rename  = require('gulp-rename');
+var gulp      = require('gulp'),
+    uglify    = require('gulp-uglify'),
+    prefix    = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css'),
+    rename    = require('gulp-rename');
 
 gulp.task('build', function() {
-    return gulp.src('source/ouibounce.js')
+    gulp.src('source/ouibounce.js')
         .pipe(uglify())
         .pipe(rename('ouibounce.min.js'))
         .pipe(gulp.dest('source'));
+
+    gulp.src('test/ouibounce.css')
+        .pipe(prefix())
+        .pipe(minifyCSS())
+        .pipe(rename('ouibounce.min.css'))
+        .pipe(gulp.dest('test'));
 });
