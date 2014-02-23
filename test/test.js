a4507633e71a08a70055f030da0a9cb8d126d4fb
carl sednaoui <carlsednaoui@gmail.com>
2014-02-23 18:39:16 -0500

    Test that OuiBounce cookies get dropped

--- test/test.js
+++ test/test.js
@@ -21,4 +21,20 @@ describe('Performs basic OuiBounce functionality', function() {
   it('should be present', function() {
     should(this.window.ouiBounce).be.type('function');
   });
+
+  it('should fire and drop cookie', function(done) {
+    // save window context
+    _this = this;
+
+    // ensure cookies are blank
+    should(_this.window.document.cookie).be.empty;
+
+    // fire OuiBounce, ensure OuiBouce cookie was dropped
+    browser
+      .fire('html', 'mouseout')
+      .then(function() {
+        should(_this.window.document.cookie).eql('viewedOuibounceModal=true');
+      })
+      .then(done);
+  });
 });
