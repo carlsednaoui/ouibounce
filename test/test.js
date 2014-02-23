c92343d8c091fdaa819c0642c98355dd985e395c
carl sednaoui <carlsednaoui@gmail.com>
2014-02-23 18:17:27 -0500

    Add 1st test :fire:

--- test/test.js
+++ test/test.js
@@ -6,5 +6,19 @@ var browser        = new Zombie(),
     test_page_path = 'file://' + path.join(__dirname, 'index.html');
 
 describe('Performs basic OuiBounce functionality', function() {
-  it('should be present');
+  before(function(done) {
+    _this = this;
+    
+    browser
+      .visit(test_page_path)
+      .then(browser.wait.bind(browser)) //bind browser to wait bc it may be out of context
+      .then(function() {
+        _this.window = browser.document.window;
+      })
+      .then(done);
+  });
+
+  it('should be present', function() {
+    should(this.window.ouiBounce).be.type('function');
+  });
 });
