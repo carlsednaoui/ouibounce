1e36a031872deeb208cbb5afa2658452df4b81f5
Jeff Escalante <hello@jenius.me>
2014-02-23 16:44:36 -0500

    refactor gulp to be cleaner and more efficient
    - adds a umd wrapper around all js files
    - pushes js files to a clean 'build' folder
    - prefixes the normal css file in place
    - remove stylesheet with only one property

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,20 @@ var gulp      = require('gulp'),
     uglify    = require('gulp-uglify'),
     prefix    = require('gulp-autoprefixer'),
     minifyCSS = require('gulp-minify-css'),
+    umd_wrap  = require('gulp-wrap-umd'),
     rename    = require('gulp-rename');
 
 gulp.task('build', function() {
+
     gulp.src('source/ouibounce.js')
+        .pipe(umd_wrap({ namespace: 'ouiBounce' }))
+        .pipe(gulp.dest('build'))
+
+    gulp.src('source/ouibounce.js')
+        .pipe(umd_wrap({ namespace: 'ouiBounce' }))
         .pipe(uglify())
         .pipe(rename('ouibounce.min.js'))
-        .pipe(gulp.dest('source'));
+        .pipe(gulp.dest('build'));
 
     gulp.src('test/ouibounce.css')
         .pipe(prefix())
@@ -18,6 +25,5 @@ gulp.task('build', function() {
 
     gulp.src('test/ouibounce.css')
         .pipe(prefix())
-        .pipe(rename('ouibounce-prefix.css'))
         .pipe(gulp.dest('test'));
 });
