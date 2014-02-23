a39af56a091093448db56074d2674558d8d5a695
carl sednaoui <carlsednaoui@gmail.com>
2014-02-23 16:43:18 -0500

    Remove jQuery dependency. Close #2
    - ouiBounce no longer uses jQuery
    - ouiBounce fn was renamed from `ouibounce` to `ouiBounce`
    - Update example in test folder
    - Update README accordingly

--- source/ouibounce.js
+++ source/ouibounce.js
@@ -1,32 +1,35 @@
-$.fn.ouibounce = function(config) {
+function ouiBounce(el, config) {
   var config      = config || {},
       aggressive  = config.aggressive || false,
       sensitivity = setDefault(config.sensitivity, 20),
       timer       = setDefault(config.timer, 1000),
       callback    = config.callback || function() {};
 
-  setTimeout(attachOuiBounce.bind(this), timer);
+  setTimeout(attachOuiBounce.bind(el), timer);
 
   function setDefault(_property, _default) {
     return typeof _property === 'undefined' ? _default : _property;
   }
 
   function attachOuiBounce() {
-    var _this = this;
-    $('html').on('mouseout.ouibounce', function(e) {
+    var _this = this,
+        _html = document.getElementsByTagName('html')[0];
+
+    _html.addEventListener('mouseout', handleMouseout);
+
+    function handleMouseout(e) {
       if (e.clientY > sensitivity || (getCookieValue('viewedOuibounceModal', 'true') && !aggressive)) return;
-      _this.show();
+      _this.style.display = 'block';
       callback();
 
       // set cookie and disable mouseout event
       document.cookie = 'viewedOuibounceModal=true';
-      $('html').off('mouseout.ouibounce');
-    });
+      _html.removeEventListener('mouseout', handleMouseout);
+    }
   }
 
   function getCookieValue(k, v) {
     // return cookies in an object
-    var cookies = document.cookie.split(';').reduce(function(prev, curr) {
     var cookies = document.cookie.split('; ').reduce(function(prev, curr) {
       var el = curr.split('=');
       prev[el[0]] = el[1];
@@ -36,4 +39,4 @@ $.fn.ouibounce = function(config) {
 
     return cookies[k] === v;
   }
-};
+}
