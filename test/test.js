a63a5a037d87a5e634aaee398b9d36094231ebb5
carl sednaoui <carlsednaoui@gmail.com>
2014-02-23 19:12:41 -0500

    Test aggressive mode

--- test/test.js
+++ test/test.js
@@ -2,20 +2,12 @@ var should = require('should'),
     Zombie = require('zombie'),
     path   = require('path');
 
-var browser        = new Zombie(),
-    test_page_path = 'file://' + path.join(__dirname, 'fixtures/basic.html');
+var browser        = new Zombie();
 
 describe('Performs basic OuiBounce functionality', function() {
+  
   before(function(done) {
-    _this = this;
-    
-    browser
-      .visit(test_page_path)
-      .then(browser.wait.bind(browser)) //bind browser to wait bc it may be out of context
-      .then(function() {
-        _this.window = browser.document.window;
-      })
-      .then(done);
+    loadPage.call(this, 'basic.html', done);
   });
 
   it('should be present', function() {
@@ -28,16 +20,69 @@ describe('Performs basic OuiBounce functionality', function() {
 
     // ensure cookies are blank
     should(_this.window.document.cookie).be.empty;
+    should(_this.window.ouiBounceCounter).equal(0);
 
     // fire OuiBounce, ensure OuiBouce cookie was dropped
     browser
       .fire('html', 'mouseout')
       .then(function() {
         should(_this.window.document.cookie).eql('viewedOuibounceModal=true');
+        should(_this.window.ouiBounceCounter).equal(1);
       })
       .then(done);
   });
 
-  it('should only fire once');
-  it('should fire multiple times if aggressive mode is on');
+  it('should only fire once', function(done) {
+    // save window context
+    _this = this;
+
+    // ensure ouiBouce already fired
+    should(_this.window.ouiBounceCounter).equal(1);
+
+    // fire OuiBounce, ensure OuiBouce does NOT go off again
+    browser
+      .fire('html', 'mouseout')
+      .then(function() {
+        should(_this.window.ouiBounceCounter).equal(1);
+      })
+      .then(done);
+  });
 });
+
+
+describe('Performs basic OuiBounce functionality', function() {
+  
+  before(function(done) {
+    loadPage.call(this, 'aggressive.html', done);
+  });
+
+  it('should fire more than once', function(done) {
+    // save window context
+    _this = this;
+
+    // ensure ouiBouce already fired
+    should(_this.window.ouiBounceCounter).equal(1);
+
+    // fire OuiBounce, ensure OuiBouce does NOT go off again
+    browser
+      .fire('html', 'mouseout')
+      .then(function() {
+        should(_this.window.ouiBounceCounter).equal(2);
+      })
+      .then(done);
+  });
+});
+
+
+function loadPage(_path, done) {
+  var _this = this,
+      test_page_path = 'file://' + path.join(__dirname, 'fixtures', _path);
+
+  browser
+    .visit(test_page_path)
+    .then(browser.wait.bind(browser)) //bind browser to wait bc it may be out of context
+    .then(function() {
+      _this.window = browser.document.window;
+    })
+    .then(done);
+}
