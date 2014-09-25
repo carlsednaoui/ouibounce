cd11fc7267994955e7fafd87a5e36d1870655117
Julien Deniau <julien.deniau@gmail.com>
2014-09-25 10:10:21 +0200

    Add cookieName configuration
    It may be usefull if you want to add multiple ouibounce on different type of pages.

--- build/ouibounce.js
+++ build/ouibounce.js
@@ -19,6 +19,7 @@ return function ouibounce(el, config) {
     callback     = config.callback || function() {},
     cookieExpire = setDefaultCookieExpire(config.cookieExpire) || '',
     cookieDomain = config.cookieDomain ? ';domain=' + config.cookieDomain : '',
+    cookieName   = config.cookieName ? config.cookieName : 'viewedOuibounceModal',
     sitewide     = config.sitewide === true ? ';path=/' : '',
     _delayTimer  = null,
     _html        = document.documentElement;
@@ -45,7 +46,7 @@ return function ouibounce(el, config) {
   }
 
   function handleMouseleave(e) {
-    if (e.clientY > sensitivity || (checkCookieValue('viewedOuibounceModal', 'true') && !aggressive)) return;
+    if (e.clientY > sensitivity || (checkCookieValue(cookieName, 'true') && !aggressive)) return;
 
     _delayTimer = setTimeout(_fireAndCallback, delay);
   }
@@ -59,7 +60,7 @@ return function ouibounce(el, config) {
 
   var disableKeydown = false;
   function handleKeydown(e) {
-    if (disableKeydown || checkCookieValue('viewedOuibounceModal', 'true') && !aggressive) return;
+    if (disableKeydown || checkCookieValue(cookieName, 'true') && !aggressive) return;
     else if(!e.metaKey || e.keyCode !== 76) return;
 
     disableKeydown = true;
@@ -73,7 +74,7 @@ return function ouibounce(el, config) {
   function parseCookies() {
     // cookies are separated by '; '
     var cookies = document.cookie.split('; ');
-    
+
     var ret = {};
     for (var i = cookies.length - 1; i >= 0; i--) {
       var el = cookies[i].split('=');
@@ -115,7 +116,11 @@ return function ouibounce(el, config) {
       cookieDomain = ';domain=' + options.cookieDomain;
     }
 
-    document.cookie = 'viewedOuibounceModal=true' + cookieExpire + cookieDomain + sitewide;
+    if (typeof options.cookieName !== 'undefined') {
+      cookieName = 'viewedOuibounceModal';
+    }
+
+    document.cookie = cookieName + '=true' + cookieExpire + cookieDomain + sitewide;
 
     // remove listeners
     _html.removeEventListener('mouseleave', handleMouseleave);
--- source/ouibounce.js
+++ source/ouibounce.js
@@ -7,6 +7,7 @@ function ouibounce(el, config) {
     callback     = config.callback || function() {},
     cookieExpire = setDefaultCookieExpire(config.cookieExpire) || '',
     cookieDomain = config.cookieDomain ? ';domain=' + config.cookieDomain : '',
+    cookieName   = config.cookieName ? config.cookieName : 'viewedOuibounceModal',
     sitewide     = config.sitewide === true ? ';path=/' : '',
     _delayTimer  = null,
     _html        = document.documentElement;
@@ -33,7 +34,7 @@ function ouibounce(el, config) {
   }
 
   function handleMouseleave(e) {
-    if (e.clientY > sensitivity || (checkCookieValue('viewedOuibounceModal', 'true') && !aggressive)) return;
+    if (e.clientY > sensitivity || (checkCookieValue(cookieName, 'true') && !aggressive)) return;
 
     _delayTimer = setTimeout(_fireAndCallback, delay);
   }
@@ -47,7 +48,7 @@ function ouibounce(el, config) {
 
   var disableKeydown = false;
   function handleKeydown(e) {
-    if (disableKeydown || checkCookieValue('viewedOuibounceModal', 'true') && !aggressive) return;
+    if (disableKeydown || checkCookieValue(cookieName, 'true') && !aggressive) return;
     else if(!e.metaKey || e.keyCode !== 76) return;
 
     disableKeydown = true;
@@ -61,7 +62,7 @@ function ouibounce(el, config) {
   function parseCookies() {
     // cookies are separated by '; '
     var cookies = document.cookie.split('; ');
-    
+
     var ret = {};
     for (var i = cookies.length - 1; i >= 0; i--) {
       var el = cookies[i].split('=');
@@ -103,7 +104,11 @@ function ouibounce(el, config) {
       cookieDomain = ';domain=' + options.cookieDomain;
     }
 
-    document.cookie = 'viewedOuibounceModal=true' + cookieExpire + cookieDomain + sitewide;
+    if (typeof options.cookieName !== 'undefined') {
+      cookieName = 'viewedOuibounceModal';
+    }
+
+    document.cookie = cookieName + '=true' + cookieExpire + cookieDomain + sitewide;
 
     // remove listeners
     _html.removeEventListener('mouseleave', handleMouseleave);
